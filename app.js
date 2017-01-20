25d8e694fefb04676c6778ddfbfcb34d068714d7
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2017-01-19 21:26:12 -0600

    changing css and adding install count to hits template

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ var algoliasearch = require('algoliasearch');
 
 var app = express();
 
-var client = algoliasearch('FP9WZYG9KK', '6f8534882bd2383dc21b40b1458091df');
-var index = client.initIndex('apps_BC');
-
 app.use(express.static('public'));
-
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/index.html'));
