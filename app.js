91c17fb2a88a535e8b2cbe7df434e5111a734b5c
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2017-01-23 17:53:19 -0600

    working basic auth

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var path = require('path');
 var algoliasearch = require('algoliasearch');
+var basicAuth = require('express-basic-auth');
+
 
 var app = express();
+
+app.use(basicAuth({
+    users: {'Bigcommerce': 'Sellmore!'},
+    challenge: true
+}));
 
 app.use(express.static('public'));
 
